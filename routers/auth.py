6ebf1884a5dd0e994484d6cef8cdc188a9de5cce
Kadavakallu Lokesh <kadavakallulokesh@gmail.com>
2024-10-11 03:47:03 +0000

    3rd commit authentication added

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
+from typing import Annotated
+from database import SessionLocal
+from sqlalchemy.orm import Session
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
 class CreateUserRequest(BaseModel):
@@ -12,6 +20,41 @@ class CreateUserRequest(BaseModel):
     password:str
     role:str
 
-@router.post("/auth")
-async def create_user(create_user_request:CreateUserRequest):
-    pass
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+db_dependency = Annotated[Session, Depends(get_db)]
+
+def authenticate_user(username:str,password:str,db):
+    user = db.query(Users).filter(Users.username==username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+@router.post("/auth", status_code = status.HTTP_201_CREATED)
+async def create_user(db:db_dependency,
+                      create_user_request:CreateUserRequest):
+    create_user_model = Users(
+        email = create_user_request.email,
+        username = create_user_request.username,
+        first_name = create_user_request.first_name,
+        last_name = create_user_request.last_name,
+        role = create_user_request.role,
+        hashed_password = bcrypt_context.hash(create_user_request.password),
+        is_active = True
+    )
+    db.add(create_user_model)
+    db.commit()
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db:db_dependency):
+    user = authenticate_user(form_data.username,form_data.password,db)
+    if not user:
+        return "Authentication Failed.."
+    return "Authentication Successful.."
