504058741e1974e539d933b7e6f56a3eb92d9ebc
Kadavakallu Lokesh <kadavakallulokesh@gmail.com>
2024-10-12 09:03:48 +0000

    8th commt: alembic implemented

---
+++ alembic/versions/ecacd36224be_create_phone_number_for_user_column.py
@@ -0,0 +1,26 @@
+"""create phone number for user column
+
+Revision ID: ecacd36224be
+Revises: 
+Create Date: 2024-10-12 08:35:10.657501
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'ecacd36224be'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column('users', sa.Column('phone_number',sa.String(), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column('users','phone_number')
--- models.py
+++ models.py
@@ -12,6 +12,7 @@ class Users(Base):
     hashed_password = Column(String)
     is_active = Column(String, default=True)
     role = Column(String)
+    phone_number = Column(String)
 
 
 class Todos(Base):
