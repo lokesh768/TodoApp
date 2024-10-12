e6564a3e08cd7901fa706c984528a97377fadb0f
Kadavakallu Lokesh <kadavakallulokesh@gmail.com>
2024-10-12 03:45:42 +0000

    7th commit: chnage password feature added

--- routers/users.py
+++ routers/users.py
@@ -24,6 +24,10 @@ db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
+class UserVerification(BaseModel):
+    password:str
+    new_password:str=Field(min_length=6)
+
 
 @router.get('/',status_code=status.HTTP_200_OK)
 async def get_user(user:user_dependency,
@@ -31,3 +35,14 @@ async def get_user(user:user_dependency,
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication Failed..')
     return db.query(Users).filter(Users.id==user.get('id')).first()
+
+@router.put('/change-password',status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user:user_dependency,db:db_dependency,user_verification:UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed..')
+    user_model = db.query(Users).filter(Users.id==user.get('id')).first()
+    if not bcrypt_context.verify(user_verification.password,user_model.hashed_password):
+        raise HTTPException(status_code=402,detail='Error on password change.')
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
